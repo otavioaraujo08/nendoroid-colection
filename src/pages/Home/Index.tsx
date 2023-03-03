769dda0bb3e1a11d69448e5f69da50553f85857c
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-03-03 11:12:39 -0300

    temp: update home page

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,3 +1,41 @@
+import { Box, Button, Typography } from "@material-ui/core";
+import { useStyles } from "./styles";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import leviAckerman from "../../assets/leviAckerman.png";
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.home}>
+      <Box className={classes.mainContentBox}>
+        <Typography className={classes.title}>Os maravilhosos</Typography>
+        <Typography className={classes.subTitle}>Nendoroids</Typography>
+
+        <Box className={classes.infosBox}>
+          <Typography className={classes.historyText}>
+            Nendoroids são figuras fofas em estilo chibi com cerca de 10 cm de
+            tamanho, baseadas em personagens de seus animes favoritos, jogos e
+            muito mais! Placas faciais e partes do corpo podem ser trocadas,
+            facilitando a criação de todos os tipos de poses e looks!
+          </Typography>
+
+          <Button
+            endIcon={<HiArrowNarrowRight />}
+            className={classes.helpButton}
+          >
+            Saber mais
+          </Button>
+        </Box>
+      </Box>
+
+      <Box className={classes.decorationFrame}>
+        <img
+          src={leviAckerman}
+          alt="Exemplo de um nendoroid"
+          className={classes.image}
+        />
+      </Box>
+    </Box>
+  );
 };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,68 @@
+import { makeStyles } from "@material-ui/core";
+import decorativeFrame from "../../assets/decorativeFrame.png";
+
+export const useStyles = makeStyles((theme) => ({
+  home: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "90vh",
+    backgroundColor: "#F0F0F0",
+    padding: "0 10%",
+  },
+  mainContentBox: {
+    alignItems: "left",
+  },
+  title: {
+    fontFamily: "PT Serif, serif",
+    fontSize: "3.5rem",
+    fontWeight: 400,
+    color: "#000000",
+  },
+  subTitle: {
+    fontFamily: "IBM Plex Serif, serif",
+    fontSize: "3.3rem",
+    fontWeight: 600,
+    color: "#D2433B",
+  },
+  infosBox: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "left",
+    maxWidth: "70%",
+    marginTop: "1.5rem",
+    marginLeft: "2rem",
+  },
+  historyText: {
+    fontFamily: "Roboto",
+    fontSize: "1.2rem",
+    fontWeight: 500,
+    color: "#000000cf",
+  },
+  helpButton: {
+    marginTop: "1rem",
+    maxWidth: "8.9rem",
+    borderRadius: "0.5rem",
+    fontFamily: "Roboto",
+    fontSize: "1rem",
+    fontWeight: 500,
+    color: "#D2433B",
+    textDecoration: "underline",
+  },
+  decorationFrame: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundImage: `url(${decorativeFrame})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    minWidth: "30rem",
+    minHeight: "90rem",
+  },
+  image: {
+    marginLeft: "3rem",
+    width: "70%",
+    height: "70%",
+  },
+}));
