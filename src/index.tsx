2a6fa6be09ac76479f81c8235cfc3dcedf2e13c2
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-03-02 21:51:21 -0300

    feat: update main component to react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { Footer } from "./Layout/Footer/Index";
+import { Header } from "./Layout/Header/Index";
+import { ErrorPage } from "./pages/ErrorPage/Index";
+import { Home } from "./pages/Home/Index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "*",
+        element: <Navigate to="/" />,
+      },
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
