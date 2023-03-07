363bce1caad85fcf2b5d677551f2f6e02923219c
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-03-07 12:09:36 -0300

    temp: update development page

--- src/pages/Home/Development/Index.tsx
+++ src/pages/Home/Development/Index.tsx
@@ -5,10 +5,25 @@ export const Development = () => {
   const classes = useStyles();
 
   return (
-    <Box>
-      <Typography className={classes.development}>
-        This will be the development
-      </Typography>
+    <Box className={classes.development}>
+      <Box className={classes.developmentText}>
+        <Typography className={classes.title}>Nendoroid Colection</Typography>
+        <Typography className={classes.subTitle}>
+          A série Nendoroid apresenta personagens fofos, figuras estilo chibi
+          que cabem na palma da mão, para que eles possam estar sempre ao seu
+          lado para apoiá-lo durante todo a agitação da vida cotidiana.
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography className={classes.opnions}>
+          As Funko Pops are in the United States, Nendoroids are Japan’s
+          ubiquitous figure brand that portrays hundreds of different characters
+          from anime, games, and movies in a cutesy, minified form. Made from
+          polyvinyl chloride (PVC), they're mass-produced in factories before
+          getting shipped to fans all around the world.
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- src/pages/Home/Development/styles.ts
+++ src/pages/Home/Development/styles.ts
@@ -2,10 +2,38 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
   development: {
-    textAlign: "center",
-    color: "gold",
-    [theme.breakpoints.down("sm")]: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    padding: "0 8%",
+    backgroundColor: "#F0F0F0",
+  },
+  developmentText: {
+    marginTop: "2rem",
+  },
+  title: {
+    fontFamily: "IBM Plex Serif, serif",
+    fontSize: "1.8rem",
+    fontWeight: 600,
+    color: "#D2433B",
+    [theme.breakpoints.between(0, 750)]: {
       fontSize: "1.5rem",
     },
+  },
+  subTitle: {
+    width: "72%",
+    fontFamily: "PT Serif, serif",
+    fontSize: "1.3rem",
+    fontWeight: 400,
+    color: "#000000",
+    [theme.breakpoints.between(0, 750)]: {
+      fontSize: "1.3rem",
+    },
+  },
+  opnions: {
+    fontFamily: "Roboto",
+    fontSize: "1.2rem",
+    fontWeight: 500,
+    color: "#000000cf",
   },
 }));
